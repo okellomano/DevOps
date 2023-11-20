b24358fa9c0f01a2f161f7b2cece394feaf0711e
okellomano <onyandookello@gmail.com>
2023-11-20 10:42:57 +0300

    solutions arcitect

--- IaC/Architect_One/web-app-modules/storage.tf
+++ IaC/Architect_One/web-app-modules/storage.tf
@@ -18,3 +18,20 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
     }
   }
 }
+
+# aws s3 backend
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "postbnd-tf-state"
+  force_destroy = true
+}
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
--- IaC/Architect_One/web-app-modules/variables.tf
+++ IaC/Architect_One/web-app-modules/variables.tf
@@ -2,34 +2,34 @@
 
 variable "region" {
   description = "Default region for provider"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "app_name" {
   description = "Name of the web application"
-  type = string
-  default = "postbnd"
+  type        = string
+  default     = "postbnd"
 }
 
 variable "environment_name" {
   description = "Deployment environment (dev/staging/production)"
-  type = string
-  default = "dev"
+  type        = string
+  default     = "dev"
 }
 
 # EC2 Variables
 
 variable "ami" {
   description = "Amazom machine image for ec2 instance"
-  type = string
-  default = "ami-011899242bb902164"
+  type        = string
+  default     = "ami-011899242bb902164"
 }
 
 variable "instance_type" {
   description = "ec2 instance type"
-  type = string
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
 }
 
 # variable "instance_name" {
@@ -41,37 +41,37 @@ variable "instance_type" {
 
 variable "bucket_prefix" {
   description = "prefix for s3 bucket for app data"
-  type = string
+  type        = string
 }
 
 # Route 53 Variables
 
 variable "domain" {
   description = "website domain name"
-  type = string
+  type        = string
 }
 
 variable "create_dns_zone" {
   description = "If true, create new route53 zone, else read existing one"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 # RDS Variables
 
 variable "db_name" {
   description = "Name of db"
-  type = string
+  type        = string
 }
 
 variable "db_user" {
   description = "username for database"
-  type = string
-  default = "idiot"
+  type        = string
+  default     = "idiot"
 }
 
 variable "db_pass" {
   description = "db password"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
